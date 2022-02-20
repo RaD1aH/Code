66ef2f51e147b8f089a2e1222df1daca6e59de4f
Dmitri Gricov <gricv12@gmail.com>
2022-02-20 08:30:50 +0300

    Add description

--- SumTheStrings.py
+++ SumTheStrings.py
@@ -1,3 +1,7 @@
+# Create a function that takes 2 integers in form of a string #
+# as an input, and outputs the sum (also as a string):        #
+# Example: (Input1, Input2 -->Output)                         #
+
 def sum_str(a, b):
     if a.isdigit() and b.isdigit():
         sum_ab = int(a) + int(b)
